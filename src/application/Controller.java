6014f8be0b67a13c5dd0f3c68b671bc12e13e1e1
jjk267 <jjk267@scarletmail.rutgers.edu>
2020-10-23 05:07:38 -0400

    Revert "Moved functionality from signer to controller"
    This reverts commit 4c519bed8659fcd1241b1092b2407c25912fcfd7.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -10,14 +10,9 @@ import java.io.File;
 import java.io.IOException;
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
@@ -26,7 +21,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Paint;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import javafx.stage.Stage;
 import netscape.javascript.JSObject;
 
 /**
@@ -53,46 +47,10 @@ public class Controller implements Initializable {
 	@FXML
 	private WebView web;
 	
-	@FXML
-	
-	
 	/**
 	 * checks whether the signature field is blank
 	 */
 	private boolean blank = true;
-	
-	/**
-	 * application stage
-	 */
-	private static Stage stage = null;
-	
-	/**
-	 * application scene
-	 */
-	private static Scene scene = null;
-	
-	/**
-	 * application control
-	 */
-	private static Controller control = null;
-	
-	/**
-	 * starts the application
-	 * @param primaryStage the application stage
-	 * @throws Exception if the stage does not start
-	 */
-	public static void start(Stage primaryStage) throws Exception {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Controller.class.getResource("display.fxml"));
-		Parent root = loader.load();
-		scene = new Scene(root);
-		control = loader.getController();
-		Controller.stage = primaryStage;
-		
-		stage.setTitle("PDF Signer");
-		stage.setScene(scene);
-		stage.show();
-	}
 	
 	/**
 	 * initializes all of the UI elements within the application
@@ -146,7 +104,6 @@ public class Controller implements Initializable {
 			             });
 			}
 		});
-		
 		
 		//initializing the signature drawing area
 		GraphicsContext g = canvas.getGraphicsContext2D();
--- src/application/Signer.java
+++ src/application/Signer.java
@@ -1,8 +1,6 @@
 package application;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -21,8 +19,7 @@ public class Signer extends Application {
 	static String filepath = null;
 
 	/**
-	 * hands control over to the controller
-	 * @param stage the primary stage of the application
+	 * @param primaryStage the primary stage of the application
 	 * @throws Exception if stage does not start
 	 */
 	@Override
@@ -39,18 +36,9 @@ public class Signer extends Application {
             alert.showAndWait();
             return;
 		}
-		Controller.start(stage);
-		
-		/*stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("display.fxml"))));
+		stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("display.fxml"))));
 		stage.setTitle("PDF Signer");
-		stage.getScene().widthProperty().addListener(
-				new ChangeListener() {
-					public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-						double width = (Double) newValue;
-						
-					}
-				});
-		stage.show();*/
+		stage.show();
 	}
 	
 	/**
